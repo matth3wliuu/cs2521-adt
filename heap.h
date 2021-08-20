b827106cb628ae06e0cd86f7a93344bb372f2c0b
Matthew Liu <40874612+matth3wliuu@users.noreply.github.com>
2021-08-20 14:49:00 +1000

    Create heap.h

---
+++ heap.h
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <stdlib.h> 
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+
+typedef struct HeapRep {
+    char *items;
+    int currSize;
+    int maxSize;
+} HeapRep;
+typedef HeapRep *Heap;
+
+Heap heapCreate(int size);
+
+void heapInsert(Heap h, char item);
+
+char heapExtract(Heap h);
+
+void heapSort(char *arr, int size);
+
+void heapDisplay(Heap h);
+
+void displayArray(char *arr, int size);
+
