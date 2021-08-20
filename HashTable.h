67a2895d48de63bbbabcee8aed2ea35eeceedff0
Matthew Liu <40874612+matth3wliuu@users.noreply.github.com>
2021-08-20 14:59:09 +1000

    Create HashTable.h

---
+++ HashTable.h
@@ -0,0 +1,24 @@
+
+// * Linear Probing HashTable ADT
+
+typedef struct HashTableRep {
+    char **items;
+    int size; 
+    int nItems;
+} HashTableRep;
+
+typedef struct HashTableRep *HashTable;
+
+HashTable hashTableCreate(int size);
+
+int hash(char *key, int size);
+
+void hashInsert(HashTable hTable, char *item);
+
+char *hashGet(HashTable hTable, char *key);
+
+void hashDelete(HashTable hTable, char *key);
+
+void freeHashTable(HashTable hTable);
+
+void displayHashTable(HashTable hTable);
